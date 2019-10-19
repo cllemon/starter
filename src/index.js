10c20a950e3af899dfd20fdaa1a4333e56534625
lemon <19805815940@163.com>
2019-10-19 18:09:49 +0800

    feat: add React-hot-loader to achieve enhanced version hot update.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import { hot } from 'react-hot-loader';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom';
 
-const App = () => <h1>Hello, world!</h1>;
+const App = hot(module)(() => {
+  const [title, setTitle] = useState('hello, world!');
+
+  const reversedTitle = () =>
+    setTitle(
+      title
+        .split('')
+        .reverse()
+        .join('')
+    );
+  return (
+    <div>
+      <h1>{title}</h1>
+      <button type='button' onClick={reversedTitle}>
+        reversed title!
+      </button>
+    </div>
+  );
+});
 
 ReactDom.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
 const path = require('path');
+const webpack = require('webpack');
+const IS_PROD = process.env.NODE_ENV === 'production';
 
 module.exports = function() {
   const baseConfig = {
-    mode: 'development',
+    mode: IS_PROD ? 'production' : 'development',
 
     entry: './src/index.js',
 
     output: {
       path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
       filename: 'bundle.js'
+    },
+
+    resolve: {
+      alias: {
+        'react-dom': '@hot-loader/react-dom' // react-hot-loader 兼容 hook 写法
+      }
     },
 
     module: {
@@ -19,8 +28,25 @@ module.exports = function() {
           loader: 'babel-loader'
         }
       ]
-    }
+    },
+
+    plugins: []
   };
+
+  if (!IS_PROD) {
+    baseConfig.devServer = {
+      contentBase: path.resolve(__dirname, 'public'),
+      historyApiFallback: true,
+      compress: true,
+      open: false,
+      port: 3000,
+      stats: 'errors-only',
+      hot: true
+      // host: '0.0.0.0',
+      // useLocalIp: true,
+    };
+    baseConfig.plugins.concat([new webpack.HotModuleReplacementPlugin()]);
+  }
 
   return baseConfig;
 };
